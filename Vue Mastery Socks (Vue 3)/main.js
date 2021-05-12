95e8f4b606ec5358d538f2f49dc111c095a37958
mokhs00 <mokhs00@naver.com>
2021-05-12 12:20:31 +0900

    add : Class & Style Binding
    v-bind를 이용한 class, style 바인딩

--- Vue Mastery Socks (Vue 3)/main.js
+++ Vue Mastery Socks (Vue 3)/main.js
@@ -3,8 +3,9 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -12,18 +13,12 @@ const app = Vue.createApp({
             ]
         }
     },
-    methods : {
-        addToCart(){            
-            this.cart +=1
+    methods: {
+        addToCart() {
+            this.cart += 1
         },
-        updateImage(variantImage){
+        updateImage(variantImage) {
             this.image = variantImage
-        },
-        removeFromCart(){
-            if (this.cart > 0) {
-                this.cart-=1    
-            }
-            
         }
-    }    
+    }
 })
