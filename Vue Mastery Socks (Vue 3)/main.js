0460d1971b5fa55c3ffe0ad4e892a520d84c1ac1
mokhs00 <mokhs00@naver.com>
2021-05-12 11:09:25 +0900

    add : Vue App import ~ mount

--- Vue Mastery Socks (Vue 3)/main.js
+++ Vue Mastery Socks (Vue 3)/main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+
+// Options object를 매개변수로 
+const app = Vue.createApp({
+    data() { // es6 shorthand
+        return {
+            product : 'Socks',
+            description : 'description'
+        }
+    }
+})
